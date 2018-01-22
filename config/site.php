bdfd6c8826dffe713dcaf72274a2c008ff6ddab3
Ross Edlin <rossedlin@gmail.com>
2018-01-22 15:23:47 +0000

    add - config/

---
+++ config/site.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ross Edlin
+ * Date: 01/09/2017
+ * Time: 15:13
+ */
+
+return [
+	/**
+	 * Theme
+	 */
+//	'theme'    => 'natural-earthy',
+//	'theme'    => 'innovative-audacious',
+//	'theme'    => 'clean-energetic',
+	'theme'    => 'gorgeous-contrast',
+
+	/**
+	 * Site
+	 */
+	'title'    => 'Ross Edlin',
+];
